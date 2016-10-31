89e3288e64be248cc8dbfbb9de1364ec714df66d
vReda <15vr@queensu.ca>
2016-10-31 19:31:06 -0400

    Update processesTree.sh

--- processesTree.sh
+++ processesTree.sh
@@ -2,12 +2,5 @@
 clear
 
 pstree -lp 1 | less | grep -Po '(?<=\()[0-9]+(?=\))' | xargs ps f -p 
-
-
-#for index in ${!PIDArray[@]}; do
-	#echo -n 'PID: ' ${PIDArray[@]:1:4}
-	#echo -n 'STARTED: #look for elements around time'
-	#echo -n 'COMMAND: #space after time'
-#done
 	
 	
