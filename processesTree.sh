d71521592f727da15a2d1e41cda6e3e7ef11b0b0
vReda <15vr@queensu.ca>
2016-10-31 23:33:32 -0400

    Update processesTree.sh
    Added Names

--- processesTree.sh
+++ processesTree.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+
+#Connor Hannough		10085081
+#Vanessa Reda 			10194381
+#Ryan Kartavicius		10155548
+
 clear
 
 pstree -lp 1 | less | grep -Po '(?<=\()[0-9]+(?=\))' | xargs ps f -p 
